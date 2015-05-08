4dd7928cd56d621b376b29a4b827a0cda87d42a9
vanessa <yu100997@gmail.com>
2015-05-08 13:20:22 -0400

    battle plan

--- statpage.js
+++ statpage.js
@@ -1,1 +1,15 @@
 console.log("whassup")
+
+/*
+  We're gonna receive 2 json blobs, minimum
+
+  1) template (universal)
+  2) personal stats (player tied)
+
+  Things to do
+  * equation parsing
+  * take the template, put the personal stats in it and display
+  * make the personal stats editable
+
+  newStat(name, base?, mod? + other stuff)
+*/
