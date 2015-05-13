61e9e1caa23366770bcbc3232d0a6e5d87f01be7
corrrin9797 <corrinoff@gmail.com>
2015-05-13 13:18:34 -0400

    Adding in parsers, work in progress

--- statpage.js
+++ statpage.js
@@ -13,3 +13,43 @@ console.log("whassup")
 
   newStat(name, base?, mod? + other stuff)
 */
+
+var stats={
+	"STR":"9001",
+	"DEX":"2390478"
+}	
+
+/*
+var check=function(string){
+	for stat in stats{
+		//if stats in string, replace all instances of stat with number
+		while (string.indexOf(stat)!=-1){
+			string=string.slice(0,string.indexOf(stat)
+		}
+	}
+}
+*/
+//Takes a string (stringOriginal) and puts stringToAdd at index, removing a certain amount (toRemove) of letters
+var stringSplice = function(stringOriginal, index, toRemove, stringToAdd){
+	tempString = stringOriginal.slice(0, index);
+	tempString += stringToAdd;
+	tempString += stringOriginal.slice(index+toRemove);
+	return tempString
+}
+
+var statSplice = function(stringOriginal, statName){
+    index = stringOriginal.indexOf(statName);
+    toRemove = statName.length;
+    stringToAdd = stat[statName];
+    stringOriginal = stringSplice(stringOriginal, index, toRemove, stringToAdd)
+    return stringOriginal
+}
+
+var statSpliceAll = function(stringOriginal, statName){
+    while (stringOriginal.indexOf(statName) != -1){
+	statSplice(stringOriginal, statName);
+    }
+}
+
+
+
