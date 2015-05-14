2f5ff2f4b59e5644284876975d44fcaa48b5f225
vanessa <yu100997@gmail.com>
2015-05-14 14:01:25 -0400

    some checking

--- statpage.js
+++ statpage.js
@@ -16,9 +16,11 @@ console.log("whassup")
 
 /*
   TODO LIST
-  * parens checking
-  * if there's nothing after an operation?
-
+  * if a number is followed immediately by a number
+    * if a string is followed immediately by a number
+      * replacing string with number followed by space to make sure they're separated
+  * merge all the checks into one function in ONE LOOP because otherwise it would just be gross
+*/
 
 //TEST STATS
 var stats={
@@ -31,30 +33,30 @@ var stats={
 ## Replacing stat names with numbers ##
 #######################################
 */
-
 //Takes a string (stringOriginal) and puts stringToAdd at index, removing a certain amount (toRemove) of letters
 var stringSplice = function(stringOriginal, index, toRemove, stringToAdd){
     tempString = stringOriginal.slice(0, index);
     tempString += stringToAdd;
     tempString += stringOriginal.slice(index+toRemove);
-    return tempString    
+    return tempString;
 }
-
+//replaces the FIRST instance of statName with its value
 var statSplice = function(stringOriginal, statName){
     index = stringOriginal.indexOf(statName);
     toRemove = statName.length;
-    stringToAdd = stats[statName];
+    stringToAdd = stats[statName] + " ";
     stringOriginal = stringSplice(stringOriginal, index, toRemove, stringToAdd)
     return stringOriginal;
 }
-
+//replaces ALL instances of statName with its value
 var statSpliceAll = function(stringOriginal, statName){
     while (stringOriginal.indexOf(statName) != -1){
 	stringOriginal = statSplice(stringOriginal, statName);
     }
     return stringOriginal;
 }
-
+//replaces all instances of all statnames from dictionary stats with their values
+//final form
 var replaceStats = function(stringOriginal){
     for (var key in stats){
 	stringOriginal = statSpliceAll(stringOriginal, key);
@@ -62,15 +64,73 @@ var replaceStats = function(stringOriginal){
     return stringOriginal;
 }
 
+/*
+#####################################
+## Checking that a string is valid ##
+#####################################
+*/
 //Checking that a string is nothing but +, -, /, *, and numbers
 var checkString = function(string){
-    for (i in string){
+    for (var i in string){
 	if ("1234567890+-*/() ".indexOf(string[i]) == -1){
 	    console.log("inval char: " + string[i]);
 	    return false;
 	}
     }
     return true;
+}
+
+//Checking parens are valid
+var checkParens = function(string){
+    parensCount = 0;
+    for (var i in string){
+	if (string[i] == "("){
+	    parensCount++;
+	}
+	else if (string[i] = ")"){
+	    parensCount--;
+	}
+	if (parensCount < 0){
+	    return false;
+	}
+    }
+    if (parensCount != 0){
+	return false;
+    }
+    return true;
+}
+
+//Checking if operations have a number or close parens before them
+var checkOpBefore = function(string){
+    var previous = "";
+    var current = "";
+    for (var i in string){
+	current = string[i];
+	if ("+-*/".indexOf(current) != -1){ //if current is an op
+	    if (previous == ""){
+		return false;
+	    }
+	    else if ("1234567890()".indexOf(previous) == -1){
+		return false;
+	    }
+	}
+	if (current != " "){
+	    previous = string[i];
+	}
+    }
+}
+
+//Checking if operations have a number or open parens after them
+var checkOpAfter = function(string){
+}
+
+//Checking operations both before and after
+//final form
+var checkOp = function(string){
+}
+
+//Checking if numbers aren't followed by another number
+var checkFollow = function(string){
 }
 
 var string = "STR STR DEX DEX DEX STR"
