bc3c21a81b7f3c49bf8ab180b149dbb1cc879255
OokamiNya <42chrono@gmail.com>
2015-05-24 16:35:16 -0400

    read from character sheet, apply modifiers

--- static/test/on.js
+++ static/test/on.js
@@ -103,38 +103,85 @@ App.CharsheetView = Marionette.ItemView.extend({
                 sheet = this.attributes.sheet;
                 module = this.attributes.module;
                 
-                charstats = {}
-                for (stat in sheet) {
-                    charstats[stat] = sheet[stat];
+                var charstat = {} //character base stats
+                var charmod = {}  //character stat modifiers
+                
+                
+                //init charstat from module
+                for (var statname in module.stats) {
+                    var stat = module.stats[statname];
+                    var newstat = {}
+                    newstat.type = stat.type;
+                    newstat.lock = ("locked" in stat) ? stat["locked"] : false;
+                    
+
+                    if (stat.type=="int") {
+                        newstat.base = 0;
+                    } else if (stat.type=="str") {
+                        newstat.base = "[STRING]";
+                    } else if (stat.type=="choice") {
+                        newstat.base = "[CHOICE]";
+                    } else {
+                        //this shouldn't happen
+                        newstat.base = "[WHAT]";
+                    }
+
+                    charstat[statname] = newstat;
                 }
 
-                for (stat in module.derivedstats) {
-                    //charstats[stat] = formulathingy(module.derivedStats[stat].formula,charstats);
-                    charstats[stat] = module.derivedstats[stat].formula;
+                //populate charstat with sheet values
+                for (var statname in sheet) {
+                    var val = sheet[statname]
+                    if (!(statname in charstat)) {
+                        charstat[statname] = {"type":"?"};
+                    }
+                    charstat[statname].base = val;
+                    
+                    if (charstat[statname].type=="choice") {
+                        var dbstat = module.stats[statname].choice[val];
+                        
+                        //unlock appropriate stats
+                        if ("unlock" in dbstat) {
+                            for (var ustat in dbstat.unlock) {
+                                ustat = dbstat.unlock[ustat]; //mfwjs
+                                charstat[ustat].lock = false;
+                            }
+                        }
+                        
+                        //add modifiers to list
+                        if ("modifier" in dbstat) {
+                            var newmod = {};
+                            for (var effname in dbstat.modifier) {
+                                var val = dbstat.modifier[effname];
+                                newmod[effname] = val;
+                            }
+                            charmod[statname] = newmod;
+                        }   
+                    }   
                 }
-
-                return strformat(module.layout, charstats);
+                
+                //generate final stats
+                var charfinal = {};
+                //copy base stats
+                for (var statname in charstat) {
+                    charfinal[statname] = charstat[statname].base;
+                }
+                
+                //apply modifiers
+                for (var modname in charmod) {
+                    mod = charmod[modname];
+                    for (var effname in mod) {
+                        effval = mod[effname];
+                        charfinal[effname] += effval;
+                    }
+                }
+                
+                
+                return strformat(module.layout, charfinal);
             }
         };
     },
 	events: {
-        /*
-		"click #namesetbutton": function(){
-            var newname = $("#nametext").val();
-            if (newname != "") {
-                this.model.set({name:newname});
-            }
-        },
-		"keydown #nametext": function(e){
-            if (e.keyCode==13) {
-                var newname = $("#nametext").val();
-                if (newname != "") {
-                    this.model.set({name:newname});
-                    this.$("#nametext").focus();
-                }
-            }
-        }
-        */
 	},
 	modelEvents: {
 		"change":function(){
